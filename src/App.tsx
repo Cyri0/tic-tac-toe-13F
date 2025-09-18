28af6e9faa1d86a43d8ede9a92c7fbdba461284c
Nemes Tamás - Burgundia <you@example.com>
2025-09-18 13:46:36 +0200

    tic-tac-toe base game

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { useState } from "react"
+import Field from "./components/Field"
+
 const App = () => {
+  const [currentPlayer, setCurrentPlayer] = useState<"o"|"x">("x")
+  const [gameField, setGameField] = useState<string[][]>([
+      ['','',''],
+      ['','',''],
+      ['','','']
+  ])
+
+  function setOwner(row: number,col:number){
+    let newGameField = [...gameField]
+    newGameField[row][col] = currentPlayer
+    setCurrentPlayer(prev => prev === 'o' ? 'x' : 'o')
+    setGameField(newGameField)
+  }
+
   return (
     <div>
-      <h1>Hello TicTacToe!</h1>
+      <section className="gameField">
+        {
+          gameField.map((row, rowIdx) => {
+            return row.map((e, colIdx) => <Field owner={e} row={rowIdx} col={colIdx} setOwner={setOwner}/>)
+          })
+        }
+      </section>
     </div>
   )
 }
