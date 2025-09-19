2e773910a21cad4dbe4e23cd763c42c13d29dd9c
Nemes Tamás - Burgundia <nemestamas94@gmail.com>
2025-09-19 14:00:56 +0200

    game functionality works (mostly...)

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -3,7 +3,6 @@ const Field = (props: {owner:string,row:number, col:number, setOwner: (row: numb
     <button disabled={props.owner !== ""} onClick={()=>props.setOwner(props.row, props.col)}>
         {props.owner === "o" && 
         <i style={{color: "var(--myorange)"}} className="fa-solid fa-o"></i>}
-
         {props.owner === "x" && 
         <i style={{color: "var(--myblue)"}}  className="fa-solid fa-xmark"></i>}        
     </button>
